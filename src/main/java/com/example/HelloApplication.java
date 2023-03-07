4f3063d7b6cb3c6faef16c63745138d183cbda29
mohammadrehan28 <mohammadre1654@gmail.com>
2023-03-07 23:36:19 +0200

    Third Commit

--- src/main/java/com/example/HelloApplication.java
+++ src/main/java/com/example/HelloApplication.java
@@ -2,6 +2,7 @@ package com.example;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.io.IOException;
@@ -11,14 +12,14 @@ public class HelloApplication extends Application {
     //SignUpSteps S;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Sign-up-view/hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
---
+++ src/test/java/org/example/AcceptableTest/ReportsSteps.java
@@ -0,0 +1,20 @@
+package org.example.AcceptableTest;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ReportsSteps {
+    @Given("I have chosen to print report screen")
+    public void iHaveChosenToPrintReportScreen() {
+
+    }
+    @When("I press on button to print a report")
+    public void iPressOnButtonToPrintAReport() {
+
+    }
+    @Then("I should see a report on jasper report and transfer to PDF")
+    public void iShouldSeeAReportOnJasperReportAndTransferToPDF() {
+
+    }
+}
